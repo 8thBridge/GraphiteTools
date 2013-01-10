f711d8dcbd0df8023ccbb8b8e195fdf79cbd18ee
mhobbs <mhobbs@8thbridge.com>
2013-01-10 15:52:20 -0600

    First pass and making mysql_output actually connect to mysql.

--- src/graphite/load/mysql_output.py
+++ src/graphite/load/mysql_output.py
@@ -1,8 +1,12 @@
-from graphite.load import AbstractOutputFormat
-from graphite.extract.base import NODE_TYPE_USER
-import sqlite3
 import sys
 
+import MySQLdb
+
+from graphite.load import AbstractOutputFormat
+from graphite.extract.base import NODE_TYPE_USER
+
+
+# TODO: create a meta schema object that can be shared by all DB outputs
 TABLES = []
 TABLES.append(('user',
 	"CREATE TABLE IF NOT EXISTS `user` ("
@@ -13,26 +17,26 @@ TABLES.append(('user',
 	"  `last_name` varchar(128),"
 	"  `profile_image` varchar(512),"
 	"  `ts` TIMESTAMP,"
-	"  PRIMARY KEY (`user_id`) ON CONFLICT REPLACE"
+	"  PRIMARY KEY (`user_id`)"
 	")")
 )
 TABLES.append(('friend',
 	"CREATE TABLE IF NOT EXISTS `friend` ("
 	"  `user_id` BIGINT UNSIGNED NOT NULL,"
 	"  `friend_id` BIGINT UNSIGNED NOT NULL,"
-	"  CONSTRAINT relation_edge UNIQUE (`user_id`, `friend_id`) ON CONFLICT IGNORE"
+	"  UNIQUE (`user_id`, `friend_id`)"
 	")")
 )
 
 TABLES.append(('object',
-	"CREATE TABLE IF NOT EXISTS `friend` ("
+	"CREATE TABLE IF NOT EXISTS `object` ("
 	"  `id` BIGINT UNSIGNED NOT NULL,"
 	"  `friend_id` BIGINT UNSIGNED NOT NULL,"
 	"  `url` varchar(512),"
 	"  `image` varchar(512),"
 	"  `title` varchar(512),"
 	"  `ts` TIMESTAMP,"
-	"  PRIMARY KEY (`id`) ON CONFLICT REPLACE"
+	"  PRIMARY KEY (`id`)"
 	")")
 )
 
@@ -44,24 +48,24 @@ TABLES.append(('action',
 	"  `created` TIMESTAMP,"
 	"  `deleted` TIMESTAMP,"
 	"  `action` varchar(32),"
-	"  PRIMARY KEY (`id`) ON CONFLICT REPLACE"
+	"  PRIMARY KEY (`id`)"
 	")")
 )
 
 
-class SqliteOutput(AbstractOutputFormat):
-	filename = None
-
-	def __init__(self, filename=None, **kwds):
-		self.filename = filename
-		if self.filename is None:
-			self.filename = "igapi-example.db"
-		self.conn = sqlite3.connect(self.filename)
+class MySQLOutput(AbstractOutputFormat):
+	def __init__(self, host=None, port=None, db=None, user=None, password=None):
+		# The MySQLdb.connect() function acts weird if we send it None kwargs
+		self.conn_kwargs = dict(host=host, port=port, db=db, user=user, passwd=password)
+		for name, value in self.conn_kwargs.items():
+			if value is None:
+				del self.conn_kwargs[name]
+		self.conn = MySQLdb.connect(**self.conn_kwargs)
 		self.create_tables()
 		self.conn.close()
 
 	def start(self, node_type):
-		self.conn = sqlite3.connect(self.filename)
+		self.conn = MySQLdb.connect(**self.conn_kwargs)
 		self.cursor = self.conn.cursor()
 
 	def handle(self, node_type, id, node):
